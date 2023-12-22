13dd7cb82e6da4825c49fcc97d78959a228e516a
OFFICE\simona.ristovska <simona.ristovska@it-labs.com>
2023-12-22 17:16:15 +0100

    initial version of project

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function AboutPage() {
+  return (
+      <main>
+        <img src="/logo.png" alt="A server surrounded by magic sparkles."/>
+        <h1>About us</h1>
+        <p>🔥 Let&apos;s get started! 🔥</p>
+      </main>
+  )
+}
